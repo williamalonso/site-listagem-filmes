f5f32479760192e3ab4cb32f4848fdc3036cfcda
“William <williamalonso@outlook.com>
2021-12-23 18:46:19 -0300

    quick fix

--- js/index.js
+++ js/index.js
@@ -67,33 +67,3 @@ function getColor(vote) {
   }
 
  });
-
-// quando der o submit vamos chamar a função "pesquisarFilme"
-/*document
-  .getElementById("formulario")
-  .addEventListener("submit", pesquisarFilme);
-function pesquisarFilme(e) {
-  var filmePesquisa = document.getElementById("pesquisar").value;
-  buscarFilmes(filmePesquisa);
-  //impede que o formulário seja enviado
-  e.preventDefault();
-}
-function buscarFilmes(filmePesquisa) {
-  axios
-    .get("http://www.omdbapi.com/?s=" + filmePesquisa)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-<<<<<<< HEAD
-}*/
-=======
-}*/
->>>>>>> 840b43484991f6f432aaf413f7e54dc768981daa
